ca718f6f4491ac328648da1e32482527408f4ff4
findech1 <1664720-findech1@users.noreply.replit.com>
2024-04-26 14:34:27 +0000

    Initial commit

---
+++ django_project/views.py
@@ -0,0 +1,41 @@
+import requests
+from django.shortcuts import render 
+
+def index(request):
+  response = requests.get('https://uselessfacts.jsph.pl/random.json?language=en')
+  data = response.json()
+  fact = data['text']
+
+  r3 = requests.get('https://dog.ceo/api/breeds/image/random')
+  res3 = r3.json()
+  dog = res3['message']
+
+
+  response2 = requests.get('https://freetestapi.com/api/v1/students') # Use this API
+  data2 = response2.json()
+  name = data2[0]['name']
+
+  return render(request, 'templates/index.html', {'fact': fact, 'dog': dog,  'name': name})
+
+def base(request):
+  return render(request, 'templates/base.html')
+def home(request):
+  return render(request, 'templates/home.html')
+def about(request):
+  return render(request, 'templates/about.html')
+  
+def dog(request):
+  r3 = requests.get('https://dog.ceo/api/breeds/image/random')
+  res3 = r3.json()
+  dog = res3['message']
+  return render(request, 'templates/dog.html', {'dog': dog})
+def fact(request):
+  response = requests.get('https://uselessfacts.jsph.pl/random.json?language=en')
+  data = response.json()
+  fact = data['text']
+  return render(request, 'templates/fact.html', {'fact' : fact})
+def student(request):
+  response2 = requests.get('https://freetestapi.com/api/v1/students') # Use this API
+  data2 = response2.json()
+  name = data2[0]['name']
+  return render(request, 'templates/student.html', {'name' : name})
